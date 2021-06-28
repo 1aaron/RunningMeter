8d817ada9ef4c689d47936b2640796e2d23e16da
aaron <aaronnek1@gmail.com>
2021-06-28 15:05:18 -0500

    updated gradle and dependencies, fixed permission issues

---
+++ app/src/main/java/com/aaron/runningmeter/extensions/FragmentExt.kt
@@ -0,0 +1,17 @@
+package com.aaron.runningmeter.extensions
+
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.aaron.runningmeter.databinding.LocationPermissionScreenBinding
+
+fun Fragment.showLocationPermissionDialog(acceptAction: () -> Unit) {
+    val dialogView = LocationPermissionScreenBinding.inflate(layoutInflater,null,false)
+    val dialog = AlertDialog.Builder(requireContext()).apply {
+        setView(dialogView.root)
+    }.create()
+    dialogView.acceptButton.setOnClickListener {
+        dialog.dismiss()
+        acceptAction()
+    }
+    dialog.show()
+}
